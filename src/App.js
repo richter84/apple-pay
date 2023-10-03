add029305b0be29b8f0a66b3dde46ae6776579ad
Richie <nncrich1984@gmail.com>
2023-10-03 10:52:54 +0100

    testing sending to braintree

--- src/App.js
+++ src/App.js
@@ -63,8 +63,9 @@ const App = () => {
       currencyCode: 'GBP',
       total: {
         label: 'ppl',
-        amount: '0.02',
-      }
+        amount: '0.03',
+      },
+      requiredBillingContactFields: ['name']
     });
 
     const session = new window.ApplePaySession(3, paymentRequest);
