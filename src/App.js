bf37111f82a547c24ab0f7f837ede185710e943e
Richie <nncrich1984@gmail.com>
2023-10-12 15:06:35 +0100

    testing sending to braintree

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ const App = () => {
           // Send payload.nonce to your server
           console.log('payload:', payload);
           console.log('event:', event);
-          alert(paymentRequest.cardholderName);
+          alert(paymentRequest.requiredBillingContactFields.cardholderName);
 
           //sendToServer(payload.nonce, paymentRequest.cardholderName);
 
